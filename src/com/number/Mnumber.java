456be1ef12137d83812b10fb1663a94b3450e9bc
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2021-07-24 16:37:27 +0530

    Added:UC2_comparing float value

--- src/com/number/Mnumber.java
+++ src/com/number/Mnumber.java
@@ -18,6 +18,19 @@ public class Mnumber {
         System.out.println(MAX+ " is the Maximum number");
 
 
+
+
+        System.out.println("Enter 3 Float numbers to compare");
+        Float a= new Float(sc.nextFloat());
+        Float b= new Float(sc.nextFloat());
+        Float c= new Float(sc.nextFloat());
+
+        Float mf =new Float ( a.compareTo(b) == 1 ? a : b);
+        Float FMAX = new Float ( mf.compareTo(c) == 1 ? mf : c);
+
+        System.out.println(FMAX+ " is the Maximum number");
+
+
         }
 
     }
