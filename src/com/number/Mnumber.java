baa7176b730c9ec41e90b91dc44d6babe5184f0e
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2021-07-26 18:21:49 +0530

    added:refactore2

--- src/com/number/Mnumber.java
+++ src/com/number/Mnumber.java
@@ -1,10 +1,36 @@
 package com.number;
 
 import java.util.Scanner;
+
+/*
+    Defining Generic Method
+ */
+class  Generic <T extends Comparable>
+
+{
+    //t type variables
+    public T a;
+    public T b;
+    public T c;
+
+    //constructer of generic
+    public Generic(T a, T b, T c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    //textMax methode which is given in qestion
+
+    public T testMax(){
+        T MAX = GenericCompareOperation.testMax(a,b,c);
+        return MAX;
+    }
+}
+
         //use class compareoption
 class GenericCompareOperation {
     //T extends comparable methode
-    public static <T extends Comparable> T compareMax(T a,T b,T c){
+    public static <T extends Comparable> T testMax(T a, T b, T c){
         T mf;
         //using ternary operter we are comparing a and b
         mf = ( a.compareTo(b) == 1 ? a : b);
@@ -25,7 +51,7 @@ public class Mnumber {
         Integer x= new Integer(sc.nextInt());
         Integer y= new Integer(sc.nextInt());
         Integer z= new Integer(sc.nextInt());
-        Integer IMAX = GenericCompareOperation.compareMax(x,y,z);
+        Integer IMAX = GenericCompareOperation.testMax(x,y,z);
         System.out.println(IMAX+ " is the Maximum Integer number");
 
 //for passing the float value
@@ -33,7 +59,7 @@ public class Mnumber {
         Float a= new Float(sc.nextFloat());
         Float b= new Float(sc.nextFloat());
         Float c= new Float(sc.nextFloat());
-        Float FMAX = GenericCompareOperation.compareMax(a,b,c);
+        Float FMAX = GenericCompareOperation.testMax(a,b,c);
         System.out.println(FMAX+ " is the Maximum Float number");
 
 //for passing the string value which is given in question
@@ -41,7 +67,7 @@ public class Mnumber {
         String p = new String("Apple");
         String q = new String("Banana");
         String r = new String("Peach");
-        String SMAX = GenericCompareOperation.compareMax(p,q,r);
+        String SMAX = GenericCompareOperation.testMax(p,q,r);
         System.out.println(SMAX+ " is the Maximum String");
 
 
